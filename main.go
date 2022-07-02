c0c32519cc44a99af58f7e1e8e2061c87581250d
namhihi237 <yentth237@gmail.com>
2022-07-03 00:46:51 +0700

    feat: init some library

--- main.go
+++ main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello World!",
+		})
+	})
+
+	r.Run(":8000") // listen and serve on  :8080
+}
